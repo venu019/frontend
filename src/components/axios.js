d73003745c34b17c90b6a5850213d3553775628b
Chelliboyina Venu Gopal <venugopalchelliboyina@gmail.com>
2025-09-21 10:20:26 +0530

    Update axios.js

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:9001/api", // adjust as needed
+  baseURL: "https://backend-m4eo.onrender.com", // adjust as needed
   auth: { username: "admin", password: "admin123" }
 });
 
